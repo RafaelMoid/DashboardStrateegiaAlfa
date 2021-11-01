21369a8cc83ec7034a33118c515cbbcff4426646
Rafael Varela <rafaelvrcr@gmail.com>
2021-11-01 16:33:54 -0300

    Comparison page starting

--- src/components/Comparacao/index.js
+++ src/components/Comparacao/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import Navbar2 from "../Navbarv2";
+import "./index.css";
 
 function Comparacao() {
     return (
         
-        <div className="desenvolvedores">
+        <div className="comparacao">
         <Navbar2 />
-        
+            <div className="wrapper">
+                <div className="containerTxt">
+                    <p>dashboard {'>'} comparação avançada</p>
+                    <h1>Comparação avançada </h1>
+                    <h3>Selecione até 5 jornadas para comprar os índices</h3>
+                </div>
+
+                <div className="comp1">
+                    <div className="jornadas">
+
+                    </div>
+                    <div div className="compProj">
+
+                    </div>
+                </div>
+                <div className="comp2">
+                    <div className="notes">
+
+                    </div>
+                    <div className="balanceProj">
+
+                    </div>
+                </div>
+            </div>
           </div>
             
        
