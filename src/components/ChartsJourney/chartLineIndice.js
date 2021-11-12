00a8e25280199b7899348e338345bb95e9ae90ca
Rafael Varela <rafaelvrcr@gmail.com>
2021-11-12 15:42:42 -0300

    Line Chart config Jornada

--- src/components/ChartsJourney/chartLineIndice.js
+++ src/components/ChartsJourney/chartLineIndice.js
@@ -1,10 +1,31 @@
 
+import {Line} from 'react-chartjs-2';
 
 const ChartLineIndice = () => {
     return (
         <div>
             <h3>Balanço índices no tempo</h3>
-
+            <Line 
+            data={{
+                labels:['Opção 1', 'Opção 2'],
+                datasets:[
+                    {
+                        label: [],
+                    data: [12,19],
+                    barThickness: 18,
+                    backgroundColor: [
+                        '#FE6E9F',
+                        '#393EA0'
+                    ]
+                },
+            ],
+            }}
+            height={40}
+            width={40}
+            options={{
+                maintainAspectRatio: false
+            }}
+            />
         </div>
     )
 }
