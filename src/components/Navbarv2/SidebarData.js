304f2e7f9f0ec9b21676f543ada3216774a0f50a
Rafael Varela <rafaelvrcr@gmail.com>
2021-12-16 02:15:08 -0300

    Navbar corrections

--- src/components/Navbarv2/SidebarData.js
+++ src/components/Navbarv2/SidebarData.js
@@ -1,61 +1,51 @@
 import React from 'react';
-import {AiOutlineBarChart, AiOutlineProject, AiFillHome } from "react-icons/ai";
+import {
+    AiOutlineBarChart,
+    AiOutlineProject,
+    AiFillHome
+} from "react-icons/ai";
 import * as FiIcons from "react-icons/fi";
-import { HiDocumentReport } from "react-icons/hi";
-import { ImProfile } from "react-icons/im";
-import { FiHexagon } from "react-icons/fi";
+import {
+    HiDocumentReport
+} from "react-icons/hi";
+import {
+    ImProfile
+} from "react-icons/im";
+import {
+    FiHexagon
+} from "react-icons/fi";
 
 
 export const SidebarData = [
-    
+
     {
         title: '',
         path: '',
-        icon: <FiHexagon size={45} />,
+        icon: < FiHexagon size = {
+            45
+        }
+        />,
         cName: 'icon'
     },
-    
+
     {
         title: ' Dashboard',
         path: '',
-        icon: <AiFillHome />,
+        icon: < AiFillHome / > ,
         cName: 'nav-text'
     },
-
-    {
-        title: ' Projetos',
-        path: '/Projetos',
-        icon: <AiOutlineProject />,
-        cName: 'nav-text'
-    },
-
-    {
-        title: ' Testes',
-        path: '/TesteComp',
-        icon: <HiDocumentReport />,
-        cName: 'nav-text'
-    }, 
 
     {
         title: ' Comparações avançadas',
         path: '/Comparacao',
-        icon: <AiOutlineBarChart />,
+        icon: < AiOutlineBarChart / > ,
         cName: 'nav-text'
     },
-
-
-
-   /* {
-        title: ' Meu perfil',
-        path: '/Desenvolvedores',
-        icon: <ImProfile />,
-        cName: 'nav-text'
-    },*/
 
     {
         title: ' Fazer logout',
         path: '/',
-        icon: <FiIcons.FiLogOut />,
+        icon: < FiIcons.FiLogOut / > ,
         cName: 'nav-text',
         click: '"handleLogout"'
     }
