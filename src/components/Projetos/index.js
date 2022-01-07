34d0a2e438e190b87a62a1e9e54a0adc31ddab3b
Rafael Varela <rafaelvrcr@gmail.com>
2022-01-07 17:42:00 -0300

    PDF Download button ok (error with design)

--- src/components/Projetos/index.js
+++ src/components/Projetos/index.js
@@ -11,10 +11,17 @@ import {fetchMapById, fetchMapStatistics ,
 import { AuthContext } from "../providers/auth";
 
 import {BsFillPersonFill} from 'react-icons/bs';
+import printJS from "print-js";
 
 function Projetos() {
 
-    
+    function printPage() {
+        printJS({
+            printable: 'printJS-form',
+            type: 'html',
+            targetStyles: ['*']
+        })
+    };
     
     //State responsavel por mostrar as visualizações do dropdown
     const [viewMode, setViewMode] = useState("indices");
@@ -88,89 +95,87 @@ function Projetos() {
         
         <div className="desenvolvedores">
         <Navbar2 />
-            <div className="mainWrapper">
-                <div className="textTitle">
-                    <p>dashboard {'>'} página de projeto</p>
-                </div>
-                <div className="resumoJornada">
-                    <img src="group96.svg" className="imgProjetcs"/>
-                    <div className="infoP">
-                        <h1>{project.title}</h1>
-                        <p className="titleP">Criada em {newDate.getDay()}/{newDate.getMonth()}/{newDate.getFullYear()}</p>
-                        <p className="titleP">Última atividade 2 dias atrás</p>
+            <div id="printJS-form">
+                <div className="mainWrapper">
+                    <div className="textTitle">
+                        <p>dashboard {'>'} página de projeto</p>
+                    </div>
+                    <div className="resumoJornada">
+                        <img src="group96.svg" className="imgProjetcs"/>
+                        <div className="infoP">
+                            <h1>{project.title}</h1>
+                            <p className="titleP">Criada em {newDate.getDay()}/{newDate.getMonth()}/{newDate.getFullYear()}</p>
+                            <p className="titleP">Última atividade 2 dias atrás</p>
+                        </div>
+                    </div>
+                    <div className="introData">
+                        {/* <h3>Modo de visualização</h3>
+                        <div className="inferior">
+                            <p>Selecione um tipo de visualização das informações da jornada</p>
+                
+                             <select className="dropdownData" onChange={handleSelection} defaultValue={viewMode}>
+                                    <option value="indices" >Índices</option>
+                                    <option value="jornadas" >Jornadas</option>
+                                    <option value="participantes" >Participantes</option>
+                                    <option value="ferramentas" >Ferramentas</option>
+                            </select>
+                        </div> */}
+                    </div>
+                    <div className="dataWrapper">
+                        <div className="data">
+                            <ChartJourney1 props={projectStatistics} props2={project}/>
+                        </div>
+                        {/* <div className="data">
+                            {viewMode === "indices" && <ChartJourney1/>}
+                            {viewMode === "jornadas" && <ChartJourney2/>}
+                            {viewMode === "participantes" && <ChartJourney1/>}
+                            {viewMode === "ferramentas" && <ChartJourney1/>}
+                
+                        </div> */}
+                
                     </div>
                 </div>
-                <div className="introData">
-                    {/* <h3>Modo de visualização</h3>
-                    <div className="inferior">
-                        <p>Selecione um tipo de visualização das informações da jornada</p>
-                        
-                         <select className="dropdownData" onChange={handleSelection} defaultValue={viewMode}>
-                                <option value="indices" >Índices</option>
-                                <option value="jornadas" >Jornadas</option>
-                                <option value="participantes" >Participantes</option>
-                                <option value="ferramentas" >Ferramentas</option>
-                        </select> 
-                    </div> */}
-                </div>
-                <div className="dataWrapper">
-                    <div className="data">
-                        <ChartJourney1 props={projectStatistics} props2={project}/>                        
+                <div className="rightBar">
+                        <PeopleContainer props={projectUsers}/>
+                        <h3 className="partTitle">Participantes mais influentes</h3>
+                        {/* MOACKADINHO DO SUCESSO */}
+                        <div className="theBestWrapper">
+                            <div className="theBest">
+                            <div className="ball"></div>
+                                <div className="person-txt-container">
+                                    <p> Matheus </p>
+                                </div>
+                            </div>
+                            <div className="theBest">
+                            <div className="ball"></div>
+                                <div className="person-txt-container">
+                                    <p> Nath </p>
+                                </div>
+                            </div>
+                            <div className="theBest">
+                            <div className="ball"></div>
+                                <div className="person-txt-container">
+                                    <p> Rafael </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bestInteractionContainer">
+                            <h3 className="bICTitle">Comentários com mais interações</h3>
+                            <p className="bICText">comentário bem longo que foi escrito na strateegia,
+                                porque eu preciso de uma exemplo para os comentários
+                                com mais concordos do rolê
+                            </p>
+                            <p>
+                            </p>
+                        </div>
+                        {/* Criar aqui um component para exibir o top 3 de pessoas
+                        com mais comentarios na plataformar que não tenham o status de admin
+                        ou habilitador */}
+                        <button className="btnProj" onClick={printPage}>
+                                Baixar relatorio
+                        </button>
                     </div>
-                    {/* <div className="data">
-                        {viewMode === "indices" && <ChartJourney1/>}
-                        {viewMode === "jornadas" && <ChartJourney2/>}
-                        {viewMode === "participantes" && <ChartJourney1/>}
-                        {viewMode === "ferramentas" && <ChartJourney1/>}
-                        
-                    </div> */}
-                    
-                </div>
             </div>
-            <div className="rightBar">
-                    <PeopleContainer props={projectUsers}/>
-                    <h3 className="partTitle">Participantes mais influentes</h3>
-                    {/* MOACKADINHO DO SUCESSO */}
-
-                    <div className="theBestWrapper">
-                        <div className="theBest">
-                        <div className="ball"></div>
-                            <div className="person-txt-container">
-                                <p> Matheus </p>
-                            </div>
-                        </div>
-                        <div className="theBest">
-                        <div className="ball"></div>
-                            <div className="person-txt-container">
-                                <p> Nath </p>
-                            </div>
-                        </div>
-                        <div className="theBest">
-                        <div className="ball"></div>
-                            <div className="person-txt-container">
-                                <p> Rafael </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="bestInteractionContainer">
-                        <h3 className="bICTitle">Comentários com mais interações</h3>
-                        <p className="bICText">comentário bem longo que foi escrito na strateegia, 
-                            porque eu preciso de uma exemplo para os comentários 
-                            com mais concordos do rolê
-                        </p>
-                        <p>
-
-                        </p>
-                    </div>
-
-                    {/* Criar aqui um component para exibir o top 3 de pessoas
-                    com mais comentarios na plataformar que não tenham o status de admin
-                    ou habilitador */}
-                    <button className="btnProj">
-                            Download PNG
-                    </button>
-                </div>
         </div>
             
        
